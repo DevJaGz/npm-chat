ae8360e2db74301bfc7c153149088338937ddc00
DevJaGz <jgomezpersonal7@gmail.com>
2024-06-18 23:06:26 -0500

    fix: set initial theme from local storage

--- src/app/components/theme-btn/theme-btn.component.ts
+++ src/app/components/theme-btn/theme-btn.component.ts
@@ -1,11 +1,12 @@
 import { DOCUMENT } from '@angular/common';
 import {
+  AfterViewInit,
   ChangeDetectionStrategy,
   Component,
-  effect,
+  Renderer2,
   inject,
 } from '@angular/core';
-import { THEME_KEY } from '@constants';
+import { DAISY_IU_THEME_DATA_KEY, THEME_KEY } from '@constants';
 import { ThemeMode } from '@models';
 import { LocalStorageService } from '@services';
 import { ThemeStore } from '@store';
@@ -18,33 +19,41 @@ import { ThemeStore } from '@store';
   styles: ``,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ThemeBtnComponent {
+export class ThemeBtnComponent implements AfterViewInit {
   readonly #themeStore = inject(ThemeStore);
   readonly #localStorage = inject(LocalStorageService);
+  readonly #renderer = inject(Renderer2);
   readonly #document = inject(DOCUMENT);
   isDarkTheme = this.#themeStore.isDarkTheme;
 
-  constructor() {
+  ngAfterViewInit(): void {
     const storedTheme = this.#localStorage.getItem<ThemeMode>(THEME_KEY);
     if (storedTheme) {
       this.#setTheme(storedTheme);
+      this.#renderTheme(storedTheme);
     }
-    effect(() => {
-      const currentTheme = this.#themeStore.selectTheme();
-      this.#setTheme(currentTheme);
-      this.#storeTheme(currentTheme);
-    });
   }
 
   toggleTheme(): void {
     this.#themeStore.toggleTheme();
+    const currentTheme = this.#themeStore.selectTheme();
+    this.#renderTheme(currentTheme);
+    this.#saveTheme(currentTheme);
   }
 
-  #setTheme(themeMode: string): void {
-    this.#document.documentElement.setAttribute('data-theme', themeMode);
+  #setTheme(themeMode: ThemeMode): void {
+    this.#themeStore.setTheme(themeMode);
   }
 
-  #storeTheme(themeMode: string): void {
+  #renderTheme(themeMode: ThemeMode): void {
+    this.#renderer.setAttribute(
+      this.#document.documentElement,
+      DAISY_IU_THEME_DATA_KEY,
+      themeMode
+    );
+  }
+
+  #saveTheme(themeMode: ThemeMode): void {
     this.#localStorage.setItem(THEME_KEY, themeMode);
   }
 }
--- src/app/constants/app.constant.ts
+++ src/app/constants/app.constant.ts
@@ -1,2 +1,3 @@
 export const APP_NAME = 'Npm Chat' as const;
 export const THEME_KEY = 'npm-chat-theme';
+export const DAISY_IU_THEME_DATA_KEY = 'data-theme';
