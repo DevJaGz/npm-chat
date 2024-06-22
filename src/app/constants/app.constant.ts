933bd6a6d6cbe1dbdc80d3997e757b72f161f97f
DevJaGz <jgomezpersonal7@gmail.com>
2024-06-21 20:19:07 -0500

    feat: set new chat description

--- src/app/constants/app.constant.ts
+++ src/app/constants/app.constant.ts
@@ -1,4 +1,4 @@
-export const APP_NAME = 'Npm Chat' as const;
+export const APP_NAME = 'Npm Chat 🤖' as const;
 export const THEME_KEY = 'npm-chat-theme';
 export const DAISY_IU_THEME_DATA_KEY = 'data-theme';
 export const TAILWIND_DARK_THEME_CLASS = 'dark';
